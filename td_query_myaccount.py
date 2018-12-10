57e8bdce084a15e90934313d28f0c86533369ef4
tluck <tjluckenbach@gmail.com>
2018-12-10 19:14:06 +0100

    fixed basic python scripts

--- td_query_myaccount.py
+++ td_query_myaccount.py
@@ -1,27 +1,15 @@
 import os
 import tdclient
+
 my_db = "evaluation_thomasluckenbach"
 apikey = os.getenv("TD_API_KEY")
+
 with tdclient.Client(apikey) as client:
     for db in client.databases():
-            print(db.tables)
+            print(" ")
             for table in db.tables():
                 print("db",table.db_name)
                 print("table",table.table_name)
                 print("count",table.count)
                 print("schema",table.schema)
 
-
-# user, host
-#    job = client.query("evaluation_thomasluckenbach", "SELECT count(*) FROM sales_data")
-    job = client.query(my_db, "SELECT count(*) FROM sales_data")
-    # sleep until job's finish
-    job.wait()
-    for row in job.result():
-        print(row)
-
-#con = tdclient.connect(db="sample_datasets")
-#cursor = con.cursor()
-#cursor.execute("SELECT user FROM www_access")
-#print cursor.fetchone()
-#print cursor.description
--- td_query_mydatabase.py
+++ td_query_mydatabase.py
@@ -1,25 +1,13 @@
 import os
 import tdclient
+
 my_db = "evaluation_thomasluckenbach"
+
 apikey = os.getenv("TD_API_KEY")
-with tdclient.Client(apikey) as client:
-    with client.database( "evaluation_thomasluckenbach" ) as db:
-       print(db.tables)
-#            for table in db.tables():
-#                print("db",table.db_name)
-#                print("table",table.table_name)
-#                print("count",table.count)
-#                print("schema",table.schema)
-# user, host
-#    job = client.query("evaluation_thomasluckenbach", "SELECT count(*) FROM sales_data")
-    job = client.query(my_db, "SELECT count(*) FROM customer_data")
+with tdclient.Client(apikey) as td:
+    job = td.query(my_db, "SELECT sum(amount) FROM sales_data where prod_id='13'")
     # sleep until job's finish
     job.wait()
+    print("Sum amount for prod_id=13")
     for row in job.result():
         print(row)
-
-#con = tdclient.connect(db="sample_datasets")
-#cursor = con.cursor()
-#cursor.execute("SELECT user FROM www_access")
-#print cursor.fetchone()
-#print cursor.description
